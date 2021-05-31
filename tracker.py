329bb30501548dd722a8df10645fc0a71689ffaa
Rohit Kumar <rohitkumar@Rohits-MacBook-Air.local>
2021-05-31 11:03:05 +0530

    add left and right side human

--- tracker.py
+++ tracker.py
@@ -1,6 +1,5 @@
 from mylib.centroidtracker import CentroidTracker
 from mylib.trackableobject import TrackableObject
-from imutils.video import VideoStream
 from imutils.video import FPS
 from mylib import config, thread
 import time
@@ -39,29 +38,25 @@ def human_detect_track():
 	# load our serialized model from disk
 	net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
 
-	# if a video path was not supplied, grab a reference to the ip camera
+	
 
-	# otherwise, grab a reference to the video file
+	
 	print("[INFO] Starting the video..")
 	vs = cv2.VideoCapture(args["input"])
 
-	# initialize the video writer (we'll instantiate later if need be)
+	
 	writer = None
 
-	# initialize the frame dimensions (we'll set them as soon as we read
-	# the first frame from the video)
+	
 	W = None
 	H = None
 
-	# instantiate our centroid tracker, then initialize a list to store
-	# each of our dlib correlation trackers, followed by a dictionary to
-	# map each unique object ID to a TrackableObject
+	
 	ct = CentroidTracker(maxDisappeared=40, maxDistance=50)
 	trackers = []
 	trackableObjects = {}
 
-	# initialize the total number of frames processed thus far, along
-	# with the total number of objects that have moved either up or down
+	
 	totalFrames = 0
 	total_left = 0
 	total_right = 0
@@ -77,19 +72,15 @@ def human_detect_track():
 
 	# loop over frames from the video stream
 	while True:
-		# grab the next frame and handle if we are reading from either
-		# VideoCapture or VideoStream
+		
 		frame = vs.read()
 		frame = frame[1] if args.get("input", False) else frame
 
-		# if we are viewing a video and we did not grab a frame then we
-		# have reached the end of the video
+		
 		if args["input"] is not None and frame is None:
 			break
 
-		# resize the frame to have a maximum width of 500 pixels (the
-		# less data we have, the faster we can process it), then convert
-		# the frame from BGR to RGB for dlib
+		
 		frame = imutils.resize(frame, width = 500)
 		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -104,21 +95,17 @@ def human_detect_track():
 			writer = cv2.VideoWriter(args["output"], fourcc, 30,
 				(W, H), True)
 
-		# initialize the current status along with our list of bounding
-		# box rectangles returned by either (1) our object detector or
-		# (2) the correlation trackers
+		
 		status = "Waiting"
 		rects = []
 
-		# check to see if we should run a more computationally expensive
-		# object detection method to aid our tracker
+		
 		if totalFrames % args["skip_frames"] == 0:
 			# set the status and initialize our new set of object trackers
 			status = "Detecting"
 			trackers = []
 
-			# convert the frame to a blob and pass the blob through the
-			# network and obtain the detections
+			
 			blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
 			net.setInput(blob)
 			detections = net.forward()
@@ -129,8 +116,7 @@ def human_detect_track():
 				# with the prediction
 				confidence = detections[0, 0, i, 2]
 
-				# filter out weak detections by requiring a minimum
-				# confidence
+				
 				if confidence > args["confidence"]:
 					# extract the index of the class label from the
 					# detections list
@@ -140,30 +126,24 @@ def human_detect_track():
 					if CLASSES[idx] != "person":
 						continue
 
-					# compute the (x, y)-coordinates of the bounding box
-					# for the object
+					
 					box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
 					(startX, startY, endX, endY) = box.astype("int")
 
 
-					# construct a dlib rectangle object from the bounding
-					# box coordinates and then start the dlib correlation
-					# tracker
+					
 					tracker = dlib.correlation_tracker()
 					rect = dlib.rectangle(startX, startY, endX, endY)
 					tracker.start_track(rgb, rect)
 
-					# add the tracker to our list of trackers so we can
-					# utilize it during skip frames
+					
 					trackers.append(tracker)
 
-		# otherwise, we should utilize our object *trackers* rather than
-		# object *detectors* to obtain a higher frame processing throughput
+		
 		else:
 			# loop over the trackers
 			for tracker in trackers:
-				# set the status of our system to be 'tracking' rather
-				# than 'waiting' or 'detecting'
+				
 				status = "Tracking"
 
 				# update the tracker and grab the updated position
@@ -179,13 +159,7 @@ def human_detect_track():
 				# add the bounding box coordinates to the rectangles list
 				rects.append((startX, startY, endX, endY))
 
-		# draw a horizontal line in the center of the frame -- once an
-		# object crosses this line we will determine whether they were
-		# moving 'up' or 'down'
 		
-
-		# use the centroid tracker to associate the (1) old object
-		# centroids with (2) the newly computed object centroids
 		objects = ct.update(rects)
 
 		# loop over the tracked objects
@@ -198,30 +172,22 @@ def human_detect_track():
 			if to is None:
 				to = TrackableObject(objectID, centroid)
 
-			# otherwise, there is a trackable object so we can utilize it
-			# to determine direction
+			
 			else:
-				# the difference between the y-coordinate of the *current*
-				# centroid and the mean of *previous* centroids will tell
-				# us in which direction the object is moving (negative for
-				# 'Left' and positive for 'right')
+				
 				y = [c[1] for c in to.centroids]
 				direction = centroid[1] - np.mean(y)
 				to.centroids.append(centroid)
 
-				# check to see if the object has been counted or not
+				
 				if not to.counted:
-					# if the direction is negative (indicating the object
-					# is moving up) AND the centroid is above the center
-					# line, count the object
+					
 					if direction < 0 and centroid[1] < H // 12:
 						total_right += 1
 						empty.append(total_right)
 						to.counted = True
 
-					# if the direction is positive (indicating the object
-					# is moving down) AND the centroid is below the
-					# center line, count the object
+					
 					elif direction > 0 and centroid[1] > H // 12:
 						total_left += 1
 						empty1.append(total_left)
@@ -271,8 +237,7 @@ def human_detect_track():
 		if key == ord("q"):
 			break
 
-		# increment the total number of frames processed thus far and
-		# then update the FPS counter
+		
 		totalFrames += 1
 		fps.update()
 
