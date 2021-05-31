143e0b7ebd8427331a3e13f5f85cf8ef50219217
Rohit Kumar <rohitkumar@Rohits-MacBook-Air.local>
2021-05-31 10:57:52 +0530

    assign id and counting human

---
+++ mylib/config.py
@@ -0,0 +1,16 @@
+#===============================================================================
+""" Optional features config. """
+#===============================================================================
+# Enter the ip camera url (e.g., url = 'http://191.138.0.100:8040/video')
+url = ''
+
+# Set max. people inside limit. Optimise number below: 10, 50, 100, etc.
+Threshold = 10
+# Threading ON/OFF
+Thread = False
+# Simple log to log the counting data
+Log = False
+# Auto stop the software after certain a time/hours
+Timer = False
+#===============================================================================
+#===============================================================================
